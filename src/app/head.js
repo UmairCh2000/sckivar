f75dd3f1190248576060524c0eae61110e58dcc3
UmairCh2000 <umair.munawar2000@gmail.com>
2024-07-05 13:25:14 +0500

    Error resolved

--- src/app/head.js
+++ src/app/head.js
@@ -1,30 +1,30 @@
 export default function Head() {
   return (
     <>
-
-<head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3M6W6582KL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3M6W6582KL');
-</script>
-    <meta name="msvalidate.01" content="43980900C326CB920B9D23B677B3B6DB" />
-    </head>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3M6W6582KL"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-3M6W6582KL');
+        </script>
+        <meta name="msvalidate.01" content="43980900C326CB920B9D23B677B3B6DB" />
+      </head>
       <title>Sckivar | Unleash Your Amazon Sales Potential</title>
-      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
-        name='title'
-        content='Sckivar | Unleash Your Amazon Sales Potential'
+        name="title"
+        content="Sckivar | Unleash Your Amazon Sales Potential"
       />
       <meta
-        name='description'
+        name="description"
         content={`We're not just another Amazon FBA agency. We're a team of dedicated experts who are obsessed with growing your sales and crushing your competitors. Let's get real and unleash your sales potential with Sckivar`}
       />
-        <meta name="msvalidate.01" content="43980900C326CB920B9D23B677B3B6DB" />
-      <link rel='icon' href='/assets/svgs/logo.svg' />
+      <meta name="msvalidate.01" content="43980900C326CB920B9D23B677B3B6DB" />
+      <link rel="icon" href="/assets/svgs/logo.svg" />
     </>
   );
 }
